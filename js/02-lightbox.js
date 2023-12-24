4660348689be92bb43f6d232eaf56c4f6e894d1e
Iskelo <iskelo.kap@gmail.com>
2023-12-24 16:47:22 +0100

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerImgs = document.querySelector('.gallery');
+
+
+const marcup = galleryItems.map(({description: desc, preview: prev, original: org}) => `
+
+
+<li class="gallery__item">
+   <a class="gallery__link" href="${org}">
+      <img class="gallery__image" src="${prev}" alt="${desc}" />
+   </a>
+</li>
+`);
+
+containerImgs.insertAdjacentHTML('beforeend', marcup.join(''));
+
+const options = {
+	captionsData: 'alt',
+	captionDelay: 250,	
+ };
+
+const lightbox = new SimpleLightbox('.gallery a', options);
