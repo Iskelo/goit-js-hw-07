95d436b822df9aa5d200d17ffaef1e2b90327977
Iskelo <iskelo.kap@gmail.com>
2023-12-25 15:41:01 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,26 +18,36 @@ const marcup = galleryItems.map(({description: desc, preview: prev, original: or
 containerImgs.insertAdjacentHTML('beforeend', marcup.join(''));
 
 containerImgs.addEventListener('click', onClick);
-document.addEventListener('keydown', onKeydown);
-
-let instance = '';
 
 function onClick(event) {
 	event.preventDefault();
 	const {source} = event.target.dataset;
 	const {alt} = event.target;
-	instance = basicLightbox.create(`
+	// console.log(event.target)
+	if (!event.target.classList.contains('gallery__image')) {
+		return;
+	}
+	const instance = basicLightbox.create(`
 	<img
 		class="gallery__image"
 		src="${source}"	
 		alt="${alt}"
 	/>
-	`);
+	`,{
+		onShow: (instance) => {
+			window.addEventListener('keydown', handleKeyDown);
+		 },
+		 onClose: (instance) => {
+			window.removeEventListener('keydown', handleKeyDown);
+		 },
+
+	});
+
 	instance.show()
+	function handleKeyDown(evt) {
+		if (evt.key === 'Escape') {
+		  instance.close();
+		}
+	 }
 }
 
-function onKeydown(evt) {	
-	if (evt.key === 'Escape') {
-		instance.close()
-	}
-}
